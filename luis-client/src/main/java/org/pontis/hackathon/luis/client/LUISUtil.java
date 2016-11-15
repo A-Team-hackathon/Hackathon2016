599e6693fc93acd5f8d92d66a0d990c0b3e7c589
A-Team-hackathon <a_team@pontis.com>
2016-11-15 23:09:58 +0200

    Multi-threading support

--- luis-client/src/main/java/org/pontis/hackathon/luis/client/LUISUtil.java
+++ luis-client/src/main/java/org/pontis/hackathon/luis/client/LUISUtil.java
@@ -6,13 +6,19 @@ import java.util.List;
 public class LUISUtil {
 	private final static String appId = "ad3678e2-9e94-4d8d-973b-4600f714a0a5";
 	private final static String appKey = "587104670a714654a2376bbd58115095";
-	private final static LUISClient client = new LUISClient(appId, appKey, true, true);
+	private final static ThreadLocal<LUISClient> client = new ThreadLocal<LUISClient>() {
+
+		@Override
+		protected LUISClient initialValue() {
+			return new LUISClient(appId, appKey, true, true);
+		}
+	};
 	private static final double THRESHOLD = 0.8;
 
 	public static String getTopIntent(String input) {
 		String result = null;
 		try {
-			LUISResponse response = client.predict(input);
+			LUISResponse response = client.get().predict(input);
 			LUISIntent topIntent = response.getTopIntent();
 			if (topIntent != null && topIntent.getScore() > THRESHOLD) {
 				result = topIntent.getName();
@@ -26,7 +32,7 @@ public class LUISUtil {
 	public static List<String> getEntities(String input) {
 		List<String> result = new ArrayList<>();
 		try {
-			LUISResponse response = client.predict(input);
+			LUISResponse response = client.get().predict(input);
 			for (LUISEntity luisEntity : response.getEntities()) {
 				if (!luisEntity.getType().startsWith("builtin") && luisEntity.getScore() > THRESHOLD) {
 					result.add(luisEntity.getType());
