866bea128c213f6283f8c8607bba695a61633d65
Nir <a_team@pontis.com>
2016-11-16 08:04:24 -0800

    fix teh codez

--- service/src/main/java/org/pontis/hackathon/service/Application.java
+++ service/src/main/java/org/pontis/hackathon/service/Application.java
@@ -1,14 +1,23 @@
 package org.pontis.hackathon.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.hackathon.fetchers.twitter.ISocialGetter;
+import org.hackathon.fetchers.twitter.TweetFetcher;
 import org.pontis.hackathon.datamodel.SocialMessage;
 
 public class Application {
 	
 	private List<ISocialGetter> socialGetters;
 	private List<DataProcessor> dataProcessors;
+	
+	public Application(){
+		socialGetters = new ArrayList<>();
+		dataProcessors = new ArrayList<>();
+		socialGetters.add(new TweetFetcher(""));
+		dataProcessors.add(new ComputeKeyPhrases());
+	}
 	
 	void doProcessing(){
 		for(final ISocialGetter socialGetter : socialGetters){
