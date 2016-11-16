f31322d8fb8a42835a578fc6791152edaee0a953
Nir <a_team@pontis.com>
2016-11-17 00:56:12 +0200

    uu

--- service/src/main/java/org/pontis/hackathon/service/Application.java
+++ service/src/main/java/org/pontis/hackathon/service/Application.java
@@ -17,8 +17,8 @@ public class Application {
 	
 	public Application(){
 		dataProcessors = new ArrayList<>();
-		dataProcessors.add(new ComputeKeyPhrases("message_keywords.csv"));
-		dataProcessors.add(new SentimentAnalysis("message_sentiment.csv"));
+		//dataProcessors.add(new ComputeKeyPhrases("message_keywords.csv"));
+		//dataProcessors.add(new SentimentAnalysis("message_sentiment.csv"));
 		dataProcessors.add(new IntentAnalysis("message_intent.csv"));
 	}
 	
